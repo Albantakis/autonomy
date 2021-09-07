708690578d7327cbc8a0170537858b4a07dc4f36
Will Mayner <wmayner@gmail.com>
2021-09-07 12:21:16 -0500

    __init__: Expose `Agent` and `LOD` in top level

--- autonomy/__init__.py
+++ autonomy/__init__.py
@@ -2,11 +2,15 @@
 ========
 autonomy
 ========
+
 Autonomy is a Python toolbox for computing measure of autonomy on
 simple artificial agents.
-If you use this software in your research, please cite the paper:
-    Albantakis (2021) Quantifying the autonomy of structurally diverse 
-    automata: acomparison of candidate measures. Forthcoming.
+
+If you use this software in your research, please cite the paper::
+
+    Albantakis (2021) Quantifying the autonomy of structurally diverse automata:
+    a comparison of candidate measures. Forthcoming.
+
 To report issues, please send an email to albantakis@wisc.edu.
 
 Usage
@@ -14,8 +18,6 @@ Usage
 The |Agent| object is the main object on which computations are performed. It
 represents the causal model of the agent as a transition probability matrix.
 """
-
-import os
 
 import pyphi
 
@@ -34,6 +36,7 @@ from . import (
 )
 from .__about__ import *  # pylint: disable=wildcard-import
 from .plotting import plot_animat_brain
+from .pyAgent import Agent, LOD
 
 __all__ = [
     "Agent",
