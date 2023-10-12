96c2585da3db1d46b8e5fe67fa488e948a3c1cd6
Paleuna <paleunahu@gmail.com>
2023-10-12 15:53:27 -0500

    new plot function

--- autonomy/plotting.py
+++ autonomy/plotting.py
@@ -2,6 +2,8 @@
 
 import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 from .structural_agent_analysis import densely_connected_nodes
 from .utils import *
@@ -50,6 +52,74 @@ def plot_animat_brain(agent, state=None, ax=None):
         node_colors[i] if node_labels[i] not in isolates else colors[0, 4]
         for i in range(n_nodes)
     ]
+
+    # indicate self-loops by thick lines
+    self_nodes_ixs = [i for i in range(n_nodes) if cm[i, i] == 1]
+    linewidths = [2.5 if i in self_nodes_ixs else 1 for i in range(n_nodes)]
+
+    # position
+    n_rows = int(np.ceil(agent.n_hidden / 2)) + 2
+
+    subset_type = np.array([0 for i in range(n_nodes)])
+    subset_type[agent.motor_ixs] = n_rows - 1
+    for c, i in enumerate(agent.hidden_ixs):
+        subset_type[i] = 1 + int(c / 2)
+
+    for c, n in enumerate(G.nodes):
+        G.nodes[n]["subset"] = subset_type[c]
+
+    pos = nx.drawing.layout.multipartite_layout(G, align="horizontal")
+
+    diff_x = 0.5
+    if len(agent.sensor_ixs) > 2:
+        for c, n in enumerate(node_labels[agent.sensor_ixs]):
+            (x, y) = pos[n]
+            if (c + 1) % 2 == 0:
+                pos[n] = (diff_x - int(c / 2) - diff_x / 2, y)
+            else:
+                pos[n] = (diff_x - int(c / 2) + diff_x / 2, y)
+
+    for c, n in enumerate(node_labels[agent.hidden_ixs]):
+        (x, y) = pos[n]
+        if ((c + 1) % 2) == 1:
+            pos[n] = (x + (int(c / 4) + 1) * diff_x * 1.2, y)
+        else:
+            pos[n] = (x - (int(c / 4) + 1) * diff_x * 1.2, y)
+
+    flipped_pos = {node: (-x, -y) for (node, (x, y)) in pos.items()}
+
+    nx.draw_networkx(
+        G,
+        with_labels=True,
+        node_size=800,
+        node_color=node_colors,
+        edgecolors="#000000",
+        linewidths=linewidths,
+        pos=flipped_pos,
+        ax=ax,
+    )
+
+def plot_animat_brain_state(agent, row_idx, ax=None):
+    n_nodes = agent.n_nodes
+    G = get_graph(agent)
+    cm = np.array(agent.cm)
+
+    print(agent.Causal_Profile_Unsorted[row_idx, 0:11], '->', agent.Causal_Profile_Unsorted[row_idx, 11:22])
+    
+    # Extract alpha values
+    alpha_values = agent.Causal_Profile_Unsorted[row_idx, 22:]
+    hidden_alpha = alpha_values[:4]
+    sensor_alpha = alpha_values[4:]
+
+    # Scale alpha values to colors
+    alpha_color_scale = plt.get_cmap('Reds')
+
+    hidden_colors = [mcolors.to_hex(alpha_color_scale(val)) if val != 0  else "#FFFFFF" for val in hidden_alpha]
+    sensor_colors = [mcolors.to_hex(alpha_color_scale(val)) if val != 0  else "#FFFFFF" for val in sensor_alpha]
+    motor_colors = ["#ffffff"] * agent.n_motors
+    node_colors = hidden_colors + sensor_colors + motor_colors
+
+    node_labels = np.array(G.nodes)
 
     # indicate self-loops by thick lines
     self_nodes_ixs = [i for i in range(n_nodes) if cm[i, i] == 1]
